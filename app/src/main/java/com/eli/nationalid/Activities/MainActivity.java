8b996c1634bbf3a0ca3b1f81c21eb9e8322afc77
=elirehema <=rocksandystone@gmail.com>
2019-10-17 14:57:09 +0300

    Added Nuxt And Vuetify

--- app/src/main/java/com/eli/nationalid/Activities/MainActivity.java
+++ app/src/main/java/com/eli/nationalid/Activities/MainActivity.java
@@ -4,17 +4,30 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ImageDecoder;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.browser.customtabs.CustomTabsIntent.Builder;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.eli.nationalid.R;
 import com.eli.nationalid.util.BaseActivity;
 import com.eli.nationalid.util.Constants;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends BaseActivity {
+    private  Bitmap icon;
+    private    ImageDecoder.Source source;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +58,53 @@ public class MainActivity extends BaseActivity {
     }
 
     private void openCustomTab() {
-        Bitmap icon = BitmapFactory.decodeResource(getResources(), android.R.drawable.ic_menu_share);
+
+
+
+
+        Builder builder = new CustomTabsIntent.Builder();
+        builder.setToolbarColor(getResources().getColor(R.color.secondaryDarkColor));
+        builder.setShowTitle(true);
+        builder.addDefaultShareMenuItem();
 
         Intent actionIntent = new Intent(this.getApplicationContext(), ShareBroadcastReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        icon = BitmapFactory.decodeResource(this.getResources(), android.R.drawable.ic_menu_share);
 
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        builder.setToolbarColor(getResources().getColor(R.color.secondaryDarkColor));
-        builder.setShowTitle(true);
-        //builder.addDefaultShareMenuItem();
+        /**
+         * Create {@link PendingIntent} and another {@link CustomTabsIntent}
+         * To open nuxtjs website
+         * **/
+        CustomTabsIntent anotherCustomTab = new CustomTabsIntent.Builder()
+                .setToolbarColor(getResources().getColor(R.color.secondaryDarkColor))
+                .addDefaultShareMenuItem()
+                .setShowTitle(true).build();
+        int requestCode = 100;
+        Intent intent = anotherCustomTab.intent;
+        intent.setData(Uri.parse(Constants.NUXT_URL));
+
+        /**
+         * Vuetify Intents
+         * **/
+        CustomTabsIntent vueTifyCustomTab = new CustomTabsIntent.Builder()
+                .setToolbarColor(getResources().getColor(R.color.secondaryDarkColor))
+                .addDefaultShareMenuItem()
+                .setShowTitle(true).build();
+        Intent vueTifyIntent = vueTifyCustomTab.intent;
+        vueTifyIntent.setData(Uri.parse(Constants.VUETIFY_URL));
+
+
+        PendingIntent pendingIntent1 = PendingIntent.getActivity(this,requestCode,intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent VUETIFY_INTENT = PendingIntent.getActivity(this,requestCode,vueTifyIntent,PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setActionButton(icon, "Share ", pendingIntent);
+        builder.addMenuItem("NuxitJs", pendingIntent1);
+        builder.addMenuItem("VuetifyJs",VUETIFY_INTENT);
         CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         customTabsIntent.launchUrl(this, Uri.parse(Constants.VUEJS_URL));
 
     }
+
 
 }
--- app/src/main/java/com/eli/nationalid/Activities/ShareBroadcastReceiver.java
+++ app/src/main/java/com/eli/nationalid/Activities/ShareBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.eli.nationalid.BuildConfig;
 import com.eli.nationalid.R;
 
 public class ShareBroadcastReceiver extends BroadcastReceiver {
@@ -16,8 +17,11 @@ public class ShareBroadcastReceiver extends BroadcastReceiver {
         }
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, url);
-        Intent chooserIntent = Intent.createChooser(shareIntent, context.getString(R.string.do_you_have_an_id_already));
+
+        String shareMessage= "\nI would like to recommend you to learn VueJs and NuxtJs in this application\n\n";
+        shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+        Intent chooserIntent = Intent.createChooser(shareIntent, context.getString(R.string.app_name));
         chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(chooserIntent);
     }
